1227f5e9863de3e06274b1dfa8445802c15097bf
MarceloXP <marceloxp@gmail.com>
2014-10-30 14:45:11 -0200

    - Cnpj Helper and Component.

---
+++ View/Helper/CnpjHelper.php
@@ -0,0 +1,14 @@
+<?php
+App::uses('Helper', 'View');
+
+class CnpjHelper extends Helper {
+
+	public $helpers = array('Mask');
+
+	public function validate($cnpj) {$cnpj = preg_replace('/[^0-9]/', '', (string) $cnpj); if (strlen($cnpj) != 14) {return false; } for ($i = 0, $j = 5, $soma = 0; $i < 12; $i++) {$soma += $cnpj{$i} * $j; $j = ($j == 2) ? 9 : $j - 1; } $resto = $soma % 11; if ($cnpj{12} != ($resto < 2 ? 0 : 11 - $resto)) {return false; } for ($i = 0, $j = 6, $soma = 0; $i < 13; $i++) {$soma += $cnpj{$i} * $j; $j = ($j == 2) ? 9 : $j - 1; } $resto = $soma % 11; return $cnpj{13} == ($resto < 2 ? 0 : 11 - $resto); }
+
+	public function mask($cnpj)
+	{
+		return ($this->validate($cnpj)) ? $this->Mask->mask($cnpj, '##.###.###/####-##') : $cnpj;
+	}
+}
